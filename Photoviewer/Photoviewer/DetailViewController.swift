8b479442554ccfdb9573b70da8cdbd37dd584096
Ganesh TR <ganeshtr24@gmzil.com>
2020-04-10 09:47:42 +0530

    Created a new project with Master detail

---
+++ Photoviewer/Photoviewer/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Photoviewer
+//
+//  Created by Ganesh TR on 10/04/20.
+//  Copyright © 2020 Ganesh TR. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
